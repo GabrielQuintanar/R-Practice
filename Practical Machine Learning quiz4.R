dcd7d550953884d54b003f5856078a09c9ae42fe
GabrielQuinmat <gquinmat@outlook.com>
2018-06-15 06:45:16 -0500

    quiz 4

--- Practical Machine Learning quiz4.R
+++ Practical Machine Learning quiz4.R
@@ -15,6 +15,34 @@ pred2 <- predict(fit2, newdata = vowel.test)
 
 confusionMatrix(pred1, vowel.test$y)$overall[1]
 confusionMatrix(pred2, vowel.test$y)$overall[1]
+confusionMatrix(pred1, pred2)$overall[1]
 
 
-predDF <- data.frame(fit, fit2, y = vowel.test$y)
+#########################################
+
+library(caret)
+
+library(gbm)
+
+set.seed(3433)
+
+library(AppliedPredictiveModeling)
+
+data(AlzheimerDisease)
+
+adData = data.frame(diagnosis,predictors)
+
+inTrain = createDataPartition(adData$diagnosis, p = 3/4)[[1]]
+
+training = adData[ inTrain,]
+
+testing = adData[-inTrain,]
+
+set.seed(62433)
+
+fit <- train(diagnosis ~ ., data = training,  method = "rf")
+fit2 <- train(diagnosis ~ ., data = training,  method = "gbm", verbose = FALSE)
+fit3 <- train(diagnosis ~ ., data = training,  method = "lda")
+
+
+predDF <- data.frame(fit, fit2, fit3, diagnosis = abData)
